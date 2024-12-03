f5e6f74a8e6442348e0c777b374d4be104dc886f
FabioOC01 <ochoacuencafabio@gmail.com>
2024-12-02 23:19:18 -0500

    Update models.py

--- api_proyectos/models.py
+++ api_proyectos/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from datetime import date
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.forms import ValidationError
 
 class CategoriaProyecto(models.Model):
     nombre = models.CharField(max_length=100)
