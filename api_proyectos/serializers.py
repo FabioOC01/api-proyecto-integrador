a6806a51b914e220e48e10d67bbe84b32c088488
FabioOC01 <ochoacuencafabio@gmail.com>
2024-12-05 10:33:51 -0500

    ultima

--- api_proyectos/serializers.py
+++ api_proyectos/serializers.py
@@ -14,6 +14,10 @@ class CategoriaProyectoSerializer(serializers.ModelSerializer):
 
 class ProyectoIntegradorSerializer(serializers.ModelSerializer):
     imagen = serializers.ImageField(use_url=True)
+    categoria = CategoriaProyectoSerializer(read_only=True)
+    año = AñoSerializer(read_only=True)
+
+
     class Meta:
         model = ProyectoIntegrador
         fields = ['id', 'titulo', 'descripcion', 'año', 'imagen', 'documento', 'video', 'url_github', 'categoria']
