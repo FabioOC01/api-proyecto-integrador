d3704f1844c17c0ca79af3d82a4a92c7ce06e7be
FabioOC01 <ochoacuencafabio@gmail.com>
2024-12-05 02:15:12 -0500

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -66,7 +66,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',  
-    'http://localhost:5173/',
+    'http://localhost:5173',
     'http://localhost:8000', 
 ]
 
