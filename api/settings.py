efc28f6591c86552450ad297cd0457afb69a2c91
FabioOC01 <ochoacuencafabio@gmail.com>
2024-12-07 03:47:16 -0500

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -57,6 +57,7 @@ MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 
