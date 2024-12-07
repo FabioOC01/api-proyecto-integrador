e0038c2ea68b60b16f47bee4fd86761db3d98f71
FabioOC01 <ochoacuencafabio@gmail.com>
2024-12-07 13:55:49 -0500

    Update views.py

--- api_proyectos/views.py
+++ api_proyectos/views.py
@@ -1,11 +1,16 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from .models import ProyectoIntegrador, CategoriaProyecto, Seccion, Año, Grupo, Alumno
 from .serializers import ProyectoIntegradorSerializer, CategoriaProyectoSerializer, AñoSerializer, GrupoSerializer, AlumnoSerializer, SeccionSerializer
+from rest_framework.parsers import MultiPartParser, FormParser
 
 class ProyectoIntegradorViewSet(viewsets.ModelViewSet):
     queryset = ProyectoIntegrador.objects.select_related('año', 'categoria').all()
     serializer_class = ProyectoIntegradorSerializer
+    parser_classes = (MultiPartParser, FormParser)  # Permite recibir archivos
 
+    def perform_update(self, serializer):
+      serializer.save()
+    
 class CategoriaProyectoViewSet(viewsets.ModelViewSet):
     queryset = CategoriaProyecto.objects.all()
     serializer_class = CategoriaProyectoSerializer
