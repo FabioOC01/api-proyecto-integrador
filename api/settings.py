02666812b3670c6fcb5ed2274a13ba2903983de7
FabioOC01 <ochoacuencafabio@gmail.com>
2024-12-07 04:10:15 -0500

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -53,7 +53,7 @@ AWS_STORAGE_BUCKET_NAME = 'tecsupvault'
 AWS_S3_REGION_NAME = 'us-east-2'
 AWS_DEFAULT_ACL = 'public-read'
 
-MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
+MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.us-east-2.amazonaws.com/'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
