b11d1079091d7b0c036f581c67172617acd64340
FabioOC01 <ochoacuencafabio@gmail.com>
2024-12-07 14:08:30 -0500

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -51,6 +51,7 @@ AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'tecsupvault'
 AWS_S3_REGION_NAME = 'us-east-2'
+AWS_QUERYSTRING_AUTH = False 
 AWS_DEFAULT_ACL = 'public-read'
 
 MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.us-east-2.amazonaws.com/'
