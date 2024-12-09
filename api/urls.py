327e87ff2e2876bbaf5dae80d8cf5dd5a1cb7b04
FabioOC01 <ochoacuencafabio@gmail.com>
2024-12-09 02:12:56 -0500

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,20 +1,11 @@
-from django.urls import path
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from api_proyectos.views import ProyectoIntegradorViewSet, CategoriaProyectoViewSet, AñoViewSet, GrupoViewSet, AlumnoViewSet, SeccionViewSet
 from django.conf import settings
 from django.conf.urls.static import static
 
-router = DefaultRouter()
-router.register(r'proyectos', ProyectoIntegradorViewSet)
-router.register(r'categorias', CategoriaProyectoViewSet)
-router.register(r'anos', AñoViewSet)
-router.register(r'grupos', GrupoViewSet)
-router.register(r'alumnos', AlumnoViewSet)
-router.register(r'secciones', SeccionViewSet)  
+from django.urls import include, path
 
 urlpatterns = [
-    path('api/', include(router.urls)),
+    # Otras rutas
+    path('auth/', include('api.urls')),  # Cambia "your_app_name" por el nombre de tu app
 ]
 
 if settings.DEBUG:
