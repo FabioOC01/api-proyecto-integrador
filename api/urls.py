de2de1b2b9cf0737dbc88e5ba3590f23882d97e5
FabioOC01 <ochoacuencafabio@gmail.com>
2024-12-09 03:13:14 -0500

    login2

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from api_proyectos.views import ProyectoIntegradorViewSet, CategoriaProyectoViewSet, AñoViewSet, GrupoViewSet, AlumnoViewSet, SeccionViewSet
 from django.conf import settings
 from django.conf.urls.static import static
+
 
 router = DefaultRouter()
 router.register(r'proyectos', ProyectoIntegradorViewSet)
@@ -10,12 +12,16 @@ router.register(r'categorias', CategoriaProyectoViewSet)
 router.register(r'anos', AñoViewSet)
 router.register(r'grupos', GrupoViewSet)
 router.register(r'alumnos', AlumnoViewSet)
-router.register(r'secciones', SeccionViewSet)  
+router.register(r'secciones', SeccionViewSet)
+
 
 urlpatterns = [
-    path('api/', include(router.urls)),
+    path('api/', include(router.urls)),  # Rutas del router
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- api_proyectos/urls.py
+++ api_proyectos/urls.py
@@ -17,7 +17,6 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/protected/', ProtectedView.as_view(), name='protected'),
 ]
 
 if settings.DEBUG:
