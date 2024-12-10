f523c030917ffcdd5dedd6187762cc8b20d71191
FabioOC01 <ochoacuencafabio@gmail.com>
2024-12-10 05:47:03 -0500

    Update serializers.py

--- api_proyectos/serializers.py
+++ api_proyectos/serializers.py
@@ -14,8 +14,9 @@ class CategoriaProyectoSerializer(serializers.ModelSerializer):
 
 class ProyectoIntegradorSerializer(serializers.ModelSerializer):
     imagen = serializers.ImageField(use_url=True)
-    categoria = serializers.PrimaryKeyRelatedField(queryset=CategoriaProyecto.objects.all())
-    año = serializers.PrimaryKeyRelatedField(queryset=Año.objects.all())
+    # Usamos los serializers anidados en lugar de solo los IDs
+    categoria = CategoriaProyectoSerializer()
+    año = AñoSerializer()
 
     class Meta:
         model = ProyectoIntegrador
@@ -23,6 +24,47 @@ class ProyectoIntegradorSerializer(serializers.ModelSerializer):
             'id', 'titulo', 'descripcion', 'año', 'imagen', 'documento', 
             'video', 'url_github', 'categoria'
         ]
+
+    def create(self, validated_data):
+        # Extraer los datos para los campos anidados
+        categoria_data = validated_data.pop('categoria')
+        año_data = validated_data.pop('año')
+
+        # Crear las instancias de los modelos anidados
+        categoria_instance = CategoriaProyecto.objects.create(**categoria_data)
+        año_instance = Año.objects.create(**año_data)
+
+        # Crear la instancia del ProyectoIntegrador con los objetos anidados
+        proyecto_integrador_instance = ProyectoIntegrador.objects.create(
+            categoria=categoria_instance,
+            año=año_instance,
+            **validated_data
+        )
+
+        return proyecto_integrador_instance
+
+    def update(self, instance, validated_data):
+        # Extraer los datos para los campos anidados
+        categoria_data = validated_data.pop('categoria', None)
+        año_data = validated_data.pop('año', None)
+
+        # Actualizar las instancias de los modelos anidados si se pasan datos nuevos
+        if categoria_data:
+            for attr, value in categoria_data.items():
+                setattr(instance.categoria, attr, value)
+            instance.categoria.save()
+
+        if año_data:
+            for attr, value in año_data.items():
+                setattr(instance.año, attr, value)
+            instance.año.save()
+
+        # Actualizar el ProyectoIntegrador
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        return instance
 
 class SeccionSerializer(serializers.ModelSerializer):
     class Meta:
