fb1345cac71ccd8f133aff5f349c3e9228b86e95
FabioOC01 <ochoacuencafabio@gmail.com>
2024-12-10 12:14:23 -0500

    Update serializers.py

--- api_proyectos/serializers.py
+++ api_proyectos/serializers.py
@@ -13,6 +13,7 @@ class CategoriaProyectoSerializer(serializers.ModelSerializer):
 
 
 class ProyectoIntegradorSerializer(serializers.ModelSerializer):
+    imagen = serializers.ImageField(use_url=True)
     categoria = CategoriaProyectoSerializer()
     año = AñoSerializer()
 
